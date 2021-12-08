e71f0e12c6361bc4b20e94d7bf812ad3361a3f87
Andre_Johnson <Andrejohnson513@gmail.com>
2021-12-07 19:57:04 -0500

    close

--- api/cars/cars-model.js
+++ api/cars/cars-model.js
@@ -1,11 +1,39 @@
+const db = require('../../data/db-config')
+
 const getAll = () => {
-  // DO YOUR MAGIC
+return db('cars')
+
 }
 
-const getById = () => {
-  // DO YOUR MAGIC
+const getById = id => {
+  return db('cars').where('id', id).first()
 }
 
-const create = () => {
-  // DO YOUR MAGIC
+const getByVin = vin => {
+  return db('cars').where('vin', vin).first()
+}
+
+const create = async car => {
+ const [id] = await db('cars').insert(car)
+ return getById(id)
+}
+
+
+
+const updateById = async (id, car) => {
+  await db('cars').where('id', id).update(car)
+  return getById(id)
+}
+
+const deleteById = id => {
+  return db('cars').where('id', id).del()
+}
+
+module.exports = {
+  getAll,
+  getById,
+  create,
+  updateById,
+  deleteById,
+  getByVin
 }
--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,7 +1,28 @@
 exports.up = function (knex) {
-  // DO YOUR MAGIC
+   knex.schema.createTable('cars', table => {
+    // primary key
+      table.increments() // defaults to 'id' if argument is omitted
+
+      table.string('make', 128) // sqlite does not enforce the char limit VARCHAR(50)
+      .notNullable()
+
+      table.interger('mileage')
+      .notNullable()
+
+      table.string('slavage', 8) 
+      .notNullable()
+
+      table.string('transmission', 10) 
+      .notNullable()
+
+      table.string('vin', 13) 
+      .unique()
+      .notNullable()
+
+    
+  })
 };
 
-exports.down = function (knex) {
-  // DO YOUR MAGIC
-};
+exports.down = async function (knex) {
+  await knex.schema.dropTableIfExists('cars')
+}
